de8f5c68d2922fde1815a15ed911a3c39f891d78
MIshkaV <mavorozhtsov_1@edu.hse.ru>
2021-09-08 13:34:27 +0300

    refactor checks in lexer

--- srcs/main_routine/lexer.c
+++ srcs/main_routine/lexer.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 16:48:37 by jbenjy            #+#    #+#             */
-/*   Updated: 2021/09/06 20:09:57 by jbenjy           ###   ########.fr       */
+/*   Updated: 2021/09/08 13:34:00 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,89 +14,53 @@
 
 static int	check_def_command_more(char *command)
 {
-	char	*curr;
-
-	curr = ft_tolower_str(command);
-	if (!ft_strcmp(curr, "unset"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "unset"))
 		return (COMMAND_UNSET);
-	}
-	if (!ft_strcmp(curr, "env"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "env"))
 		return (COMMAND_ENV);
-	}
-	if (!ft_strcmp(curr, "exit"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "exit"))
 		return (COMMAND_EXIT);
-	}
-	if (!ft_strcmp(curr, "./minishell"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "./minishell"))
 		return (COMMAND_MINISHELL);
-	}
-	free(curr);
 	return (-1);
 }
 
 
 int check_default_command(char *command)
 {	
-	char	*curr;
-
-	curr = ft_tolower_str(command);
-	if (!ft_strcmp(curr, "echo"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "echo"))
 		return (COMMAND_ECHO);	
-	}
-	if (!ft_strcmp(curr, "cd"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "cd"))
 		return (COMMAND_CD);
-	}
-	if (!ft_strcmp(curr, "pwd"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "pwd"))
 		return (COMMAND_PWD);
-	}
-	if (!ft_strcmp(curr, "export"))
-	{
-		free(curr);
+	if (!ft_strcmp(command, "export"))
 		return (COMMAND_EXPORT);
-	}
-	free(curr);
 	return (check_def_command_more(command));
 }
 
 int		check_path_command(char *command, t_vars *vars)
 {
 	struct stat buff;
-	char	*curr;
 	char	*path;
 	char	*full_path;
 	int		i;
 
-	curr = ft_tolower_str(command);
 	i = 0;
 	while (vars->paths[i])
 	{
-		path = ft_concat("/", curr);
+		path = ft_concat("/", command);
 		full_path = ft_concat(vars->paths[i], path);
 		if (stat(full_path, &buff) != -1)
 		{
 			free(path);
 			free(full_path);
-			free(curr);
 			return (i);
 		}
 		free(path);
 		free(full_path);
 		i++;
 	}
-	free(curr);
 	return (-1);
 }
 
@@ -104,16 +68,21 @@ char    *lexer_check_command(t_raw *curr, t_vars *vars)
 {
 	(void)vars;
 	int	command_code;
+	char *to_lower;
+	
 
-	command_code = check_default_command(curr->command);
+	to_lower = ft_tolower_str(curr->command);
+	command_code = check_default_command(to_lower);
 	if (command_code == -1)
 	{	
-		command_code = check_path_command(curr->command, vars);
+		command_code = check_path_command(to_lower, vars);
 		
 		if (command_code == -1)
-			print_error(ERROR_NOT_FOUND);	
-		// printf("%s\n",vars->paths[command_code]);	
+			print_error(ERROR_NOT_FOUND);
 	}
+
+	
+	free(to_lower);
 	return (0);
 }
 
