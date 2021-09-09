c1b1d4250eb38ceb3ffa219c9de1e7455c680858
MIshkaV <mavorozhtsov_1@edu.hse.ru>
2021-09-09 19:36:48 +0300

    need refactor and complete dquotes

--- srcs/main_routine/lexer.c
+++ srcs/main_routine/lexer.c
@@ -6,24 +6,13 @@
 /*   By: jbenjy <jbenjy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 16:48:37 by jbenjy            #+#    #+#             */
-/*   Updated: 2021/09/09 12:43:12 by jbenjy           ###   ########.fr       */
+/*   Updated: 2021/09/09 19:27:58 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/////////
-// char	*lexer_parse_quote(char *str)
-// {
-	
-// }
-
-// char	*lexer_parse_dquote(char *str)
-// {
-	
-// }
-
-char	*lexer_get_dollar(t_envp_list *envp, char *str)
+char	*lexer_get_dollar(t_envp_list *envp, char *str, int dquote)
 {
 	char	*curr;
 	char	*result;
@@ -34,8 +23,12 @@ char	*lexer_get_dollar(t_envp_list *envp, char *str)
 		return (0);
 	if(str[i] == '$')
 		str++;
-	while (str[i] && !is_space(str[i]))
-		i++;
+	if (dquote)
+		while (str[i] && !is_space(str[i]) && str[i] != '\"')
+			i++;
+	else
+		while (str[i] && !is_space(str[i]))
+			i++;
 	curr = ft_strndup(str, i);
 	result = ft_strdup(envp_get_data(envp, curr));
 	free(curr);
@@ -60,13 +53,67 @@ char	*lexer_parse_text(t_raw *curr, t_vars *vars, char *str)
 	char	*result;
 	
 	if(str[0] == '$')
-		result = lexer_get_dollar(vars->envp, str);
+		result = lexer_get_dollar(vars->envp, str, 0);
 	else
 		result = lexer_get_text(str);
 	curr->treated_comnd = trls_push_node(curr->treated_comnd, result);
 	return (skip_sym(str));
 }
 
+char	*lexer_parse_quote(t_raw *curr, char *str)
+{
+	int i;
+
+	i = 0;
+	if (*str == '\'')
+		str++;
+	while (str[i] && str[i] != '\'')
+		i++;
+	curr->treated_comnd = trls_push_node(curr->treated_comnd, ft_strndup(str, i));
+	return (str + i + 1);
+}
+
+char	*lexer_parse_dquote(t_raw *curr, t_vars *vars, char *str)
+{
+	int		i;
+	char	*result;
+	char	*str1;
+	char	*str2;
+	char	*path;
+	
+	
+	i = 0;
+	result = 0;
+	if (*str == '\"')
+		str++;
+	while (str[i] && str[i] != '\"')
+	{
+		if (str[i] == '$')
+		{
+			path = lexer_get_dollar(vars->envp, str + i, 1);
+			if (path)
+			{
+				result = ft_concat(result,  path);
+				free(path);
+			}
+			while (str[i] && !is_space(str[i]) && str[i] != '\"')
+				i++;
+		}
+		// else if (str[i] == '\\')
+		// {
+			
+		// }
+		else
+			i++;
+	}
+	str1 = result;
+	str2 = ft_strndup(str, i);
+	result = ft_concat(str1, str2);
+	free(str1);
+	free(str2);
+	curr->treated_comnd = trls_push_node(curr->treated_comnd, result);
+	return (str + i + 1);
+}
 
 void	lexer_parse_arg(t_raw *curr, t_vars *vars)
 {
@@ -78,13 +125,14 @@ void	lexer_parse_arg(t_raw *curr, t_vars *vars)
 		while (*str)
 		{
 			str = skip_spaces(str);
-			// if (*str = "\'")
-			// 	str = lexer_parse_quote(str);
-			// else if (*str = "\"")
-			// 	str = lexer_parse_dquote(str);
-			// else
+			if (*str == '\'')
+				str = lexer_parse_quote(curr, str);
+			else if (*str == '\"')
+				str = lexer_parse_dquote(curr, vars, str);
+			else if (*str != '\0')
 				str = lexer_parse_text(curr, vars, str);
 		}
+		// printf("|%d|\n", trls_get_len(curr->treated_comnd));
 		trls_print_list(curr->treated_comnd);
 	}		
 }
--- srcs/utils/libft_more.c
+++ srcs/utils/libft_more.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 14:12:24 by jbenjy            #+#    #+#             */
-/*   Updated: 2021/09/09 12:33:16 by jbenjy           ###   ########.fr       */
+/*   Updated: 2021/09/09 18:59:21 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,17 +66,19 @@ char	*ft_concat(char *s1, char *s2)
 	char	*str;
 	char	*begin;
 
+	if (!s1)
+		s1  = "";
+	if (!s2)
+		s2	= "";
 	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (0);
-
 	begin = str;
 	while(*s1)
 		*str++ = *s1++;
 	while(*s2)
 		*str++ = *s2++;
 	*str = '\0';
-	 
 	return (begin);
 }
 
