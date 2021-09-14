ea09f6f5b6ae3842ab972319a13124d6cd88ef6c
Jefferey Benjy <jbenjy@mi-i5.msk.21-school.ru>
2021-09-14 16:49:43 +0300

    fix lexer

--- srcs/main_routine/parser.c
+++ srcs/main_routine/parser.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 12:55:05 by jbenjy            #+#    #+#             */
-/*   Updated: 2021/09/08 15:44:32 by jbenjy           ###   ########.fr       */
+/*   Updated: 2021/09/14 16:48:22 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,33 @@ char	*find_arg(char *str, t_raw *commands)
 	return (str + curr);
 }
 
+static char *find_argv_more(char *str, t_raw *command)
+{
+	int			i;
+	char		*buff1;
+	char		*buff2;
+	char		*new_arg;
+
+	i = 0;
+	if (!is_special(str[i]))
+	{
+		while (str[i] && !is_special(str[i]))
+			i++;
+		buff1 = command->argument;
+		buff2 = ft_concat(command->argument, " ");
+		new_arg = ft_strndup(str, i);
+		command->argument = ft_concat(buff2, new_arg);
+		free(buff1);
+		free(buff2);
+		free(new_arg);
+	}
+	return (str + i);
+}
+
 char	*find_pipe(char *str, t_raw *command, int *check_type)
 {
-	int i;
-	t_redirect *curr_rct;
+	int			i;
+	t_redirect	*curr_rct;
 
 	while (*str && is_special(*str))
 	{
@@ -68,12 +91,14 @@ char	*find_pipe(char *str, t_raw *command, int *check_type)
 		curr_rct = rct_new_node();
 		str = check_rct(curr_rct, str);
 		str = skip_spaces(str);
-		while (str[i] && !is_special(str[i]))
+		while (str[i] && !is_space(str[i]) && !is_special(str[i]))
 			i++;
-		
 		curr_rct->file = ft_strndup(str, i);
 		command->redirects = rct_push(command->redirects, curr_rct);
+		while (str[i] && is_space(str[i]))
+				i++;
 		str += i;
+		str = find_argv_more(str, command);
 	}
 	return (str);
 }
